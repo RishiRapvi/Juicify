c847a678b9aacd1528e692c8e7dc6bddb5fef6d0
RishiRapvi <90780199+RishiRapvi@users.noreply.github.com>
2022-11-30 12:56:06 -0600

    adding scroller

--- app/src/main/java/com/example/juicify/Songs.java
+++ app/src/main/java/com/example/juicify/Songs.java
@@ -3,10 +3,12 @@ package com.example.juicify;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +24,52 @@ public class Songs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
 
+        SeekBar sb = (SeekBar) findViewById(R.id.seekBar);
+        Button bt_stop = (Button) findViewById(R.id.playSong);
+        final TextView tv = (TextView)findViewById(R.id.textView);
+
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                                          @Override
+                                          public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                                              int millisec_time = (i*60)/1000;
+
+                                              new CountDownTimer(millisec_time, 1000) {
+
+                                                  public void onTick(long millisUntilFinished) {
+                                                      tv.setText("seconds remaining: " + millisUntilFinished / 1000);
+                                                  }
+
+                                                  public void onFinish() {
+                                                      tv.setText("done!");
+                                                  }
+                                              }.start();
+
+
+                                          }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+//                Toast.makeText(getApplicationContext(),"GOT",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+//                Toast.makeText(getApplicationContext(),"OFF",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+        bt_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
         Intent intent = getIntent();
+
 
         try {
             // This gets the particular Detailed object we are displaying
@@ -37,7 +84,7 @@ public class Songs extends AppCompatActivity {
             TextView era = findViewById(R.id.eraTextView);
             ImageView photo = findViewById(R.id.imageView);
 
-            button =  findViewById(R.id.playSong);
+            button =  findViewById(R.id.pause);
             // set values on the screen based on the object that was passed to this Detail activity
             song.setText(juice.getSong());
             era.setText(juice.getEra());
@@ -68,6 +115,10 @@ public class Songs extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void pauseSong(View v){
+
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
